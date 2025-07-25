6c552d81e3c2a342727e9e7f841ab2940732bd0a
sola <466010106@qq.com>
2025-07-25 13:50:57 +0800

    update page leader

--- src/pages/blog/article.rs
+++ src/pages/blog/article.rs
@@ -3,7 +3,7 @@ use std::cmp::{max, min};
 use leptos::prelude::*;
 use leptos_router::hooks::use_params_map;
 
-use crate::{api::blog::{get_article, get_articles_list}, components::ui::card::{ArticleCard, ArticleInfoCard}, models::blog::ArticleInfo, state::use_app};
+use crate::{api::blog::{get_article, get_articles_list}, components::ui::card::{ArticleCard, ArticleInfoCard}, state::use_app};
 
 #[component]
 pub fn ArticleList() -> impl IntoView {
@@ -12,10 +12,10 @@ pub fn ArticleList() -> impl IntoView {
     let article_list = move || async_data.get();
     let current_page = move || state.current_page.get();
     let items_per_page = move || state.items_per_page.get();
-    let total_pages = move || {
+    let total_pages = Memo::new(move |_| {
         let articles = article_list().unwrap();
         articles.len() / items_per_page() + (articles.len() % items_per_page() > 0) as usize + (articles.len() == 0) as usize
-    };
+    });
     view! {
         <Show when=move || 
             article_list().is_some()
@@ -38,67 +38,58 @@ pub fn ArticleList() -> impl IntoView {
                         }
                     />
                 </div>
-                <div>
-                    <Show when=move || (article_list().unwrap().len() > items_per_page())>
-                        <div class="flex justify-center gap-4">
-                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 rounded" on:click=move |_| {
-                                    state.current_page.update(|page| *page = max(*page - 1, 1));
-                            }>
-                                <p>"Back"</p>
-                            </button>
-                            <For 
-                                each=move || {
-                                    let start = if current_page() > 2 { 
-                                        current_page() - 2 
-                                    } else { 
-                                        1 
-                                    };
-                                    let end = if current_page() + 2 <= total_pages() { 
-                                        current_page() + 2 
-                                    } else { 
-                                        total_pages() 
-                                    };
-                                    start..=end
-                                }
-                                key=|page| { page.clone() }
-                                children=move |page| {
-                                    let is_current = page == current_page();
-                                    let bg_color = if is_current { 
-                                        "bg-blue-200" 
-                                    } else { 
-                                        "bg-white" 
-                                    };
-                                    
-                                    view! {
-                                        <button 
-                                            class={"px-4 py-2 mx-1 text-gray-700 border border-gray-300 rounded-md hover:bg-gray-100 focus:outline-none focus:ring focus:ring-gray-300 ".to_string() + bg_color}
-                                            on:click=move |_| {
-                                                state.current_page.set(page);
-                                            }>
-                                            {page}
-                                        </button>
-                                    }
-                                    
-                                }
-                            />
-                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 rounded" on:click=move |_| {
-                                    state.current_page.update(|page| *page = min(*page + 1, total_pages()));
-                            }>
-                                <p>"Next"</p>
-                            </button>
-                        </div>
-                    </Show>
-                </div>
+                <PageLeader total_pages=total_pages/>
             </div>
         </Show>
     }
 }
 
 #[component]
-fn PageLeader() -> impl IntoView{
-
+fn PageLeader(
+    total_pages: Memo<usize>
+) -> impl IntoView{
+    let state = use_app();
+    let current_page = move || state.current_page.get();
     view! {
-
+        <div>
+            <Show when=move || (total_pages.get() > 1)>
+                <div class="flex justify-center gap-4">
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 rounded" on:click=move |_| {
+                        state.current_page.update(|page| *page = max(*page - 1, 1));
+                    }>
+                        <p>"Back"</p>
+                    </button>
+                    <For 
+                        each=move || {
+                            let start = if current_page() > 2 { current_page() - 2 } else { 1 };
+                            let end = if current_page() + 2 <= total_pages.get() { current_page() + 2 } else { total_pages.get() };
+                            start..=end
+                        }
+                        key=|page| { page.clone() }
+                        children=move |page| {
+                            let is_current = page == current_page();
+                            let bg_color = if is_current { "bg-blue-200" } else { "bg-white" };
+                            
+                            view! {
+                                <button 
+                                    class={"px-4 py-2 mx-1 text-gray-700 border border-gray-300 rounded-md hover:bg-gray-100 focus:outline-none focus:ring focus:ring-gray-300 ".to_string() + bg_color}
+                                    on:click=move |_| {
+                                        state.current_page.set(page);
+                                    }>
+                                    {page}
+                                </button>
+                            }
+                            
+                        }
+                    />
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 rounded" on:click=move |_| {
+                            state.current_page.update(|page| *page = min(*page + 1, total_pages.get()));
+                    }>
+                        <p>"Next"</p>
+                    </button>
+                </div>
+            </Show>
+        </div>
     }
 }
 
